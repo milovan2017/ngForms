921f5c1cbc1844b07d35c677b1a36beb80a5a39c
Milovan Lazovic <milovan.lazovic@quadro-tnation.com>
2018-06-11 14:25:37 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Form01Component } from './form01/form01.component';
+import { Form02Component } from './form02/form02.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Form01Component,
+    Form02Component
   ],
   imports: [
     BrowserModule
